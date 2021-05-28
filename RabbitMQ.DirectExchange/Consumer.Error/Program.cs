47c33431c2243a11f41a7464f35ffdf361eba20e
developer.stories <developer.stories.mail@gmail.com>
2021-05-28 12:04:00 +0300

    Work with Direct Exchange

---
+++ RabbitMQ.DirectExchange/Consumer.Error/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer.Error
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "direct_logs", type: ExchangeType.Direct);
+
+                var queueName = channel.QueueDeclare().QueueName;
+
+                channel.QueueBind(queue: queueName,
+                                         exchange: "direct_logs",
+                                         routingKey: "error");
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += (sender, e) =>
+                {
+                    var body = e.Body;
+                    var message = Encoding.UTF8.GetString(body.ToArray());
+                    Console.WriteLine(" Received message: {0}", message);
+                };
+
+                channel.BasicConsume(queue: queueName,
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine($"Subscribed to the queue '{queueName}'");
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
