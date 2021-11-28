63d2f7f96006a891845ad0a52503f6380b4906e0
developer.stories <developer.stories.contact@gmail.com>
2021-11-28 15:01:34 +0300

    Work with Fanout Exchange

---
+++ RabbitMQ.FunoutExchange/Producer/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            do
+            {
+                int timeToSleep = random.Next(1000, 3000);
+                Thread.Sleep(timeToSleep);
+
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange: "notifier", type: ExchangeType.Fanout);
+
+                    var moneyCount = random.Next(1000, 10_000);
+                    string message = $"Payment received for the amount of {moneyCount}";
+
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "notifier",
+                                        routingKey: "",
+                                        basicProperties: null,
+                                        body: body);
+
+                    Console.WriteLine($"Payment received for amount of {moneyCount}.\nNotifying by 'notifier' Exchange");
+                }
+            } while (true);
+        }
+    }
+}
