cd5e7e497a82b2d15dd21d1e5ad3caada5a7aea1
Alexey <developer.stories.mail@gmail.com>
2023-02-05 19:37:42 +0100

    Work with Headers Exchange

---
+++ RabbitMQ.HeadersExchange/Consumer.All.Usd/Program.cs
@@ -0,0 +1,54 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Consumer.All.Usd
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.Title = "Consumer: Counts all USD transfers";
+
+            int counter = 0;
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "headers_ex", type: ExchangeType.Headers);
+                Dictionary<string, object> headers = new Dictionary<string, object>();
+                headers.Add("x-match", "any"); //we can ignore that header if it's "any"
+                headers.Add("currency", "usd");
+
+                var queueName = channel.QueueDeclare().QueueName;
+
+                channel.QueueBind(queue: queueName,
+                                         exchange: "headers_ex",
+                                         routingKey: "-",
+                                         arguments: headers);
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += (sender, e) =>
+                {
+                    var body = e.Body;
+                    var message = Encoding.UTF8.GetString(body.ToArray());
+
+                    counter++;
+
+                    Console.WriteLine($"Received message: {message} {Environment.NewLine}" +
+                        $"Total USD transfers: {counter}");
+                };
+
+                channel.BasicConsume(queue: queueName,
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine($"Subscribed to the queue '{queueName}'");
+                Console.WriteLine($"Counts all USD transfers");
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ RabbitMQ.HeadersExchange/Producer/Program.cs
@@ -0,0 +1,119 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace Producer
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.Title = "Produser";
+
+            Thread.Sleep(2000);
+            int count = 0;
+            Task.Run(() =>
+            {
+                do
+                {
+                    int timeToSleep = new Random().Next(2000, 7000);
+                    Thread.Sleep(timeToSleep);
+
+                    var factory = new ConnectionFactory() { HostName = "localhost" };
+                    using (var connection = factory.CreateConnection())
+                    using (var channel = connection.CreateModel())
+                    {
+                        channel.ExchangeDeclare(exchange: "headers_ex", type: ExchangeType.Headers);
+
+                        Dictionary<string, object> headers = new Dictionary<string, object>();
+                        headers.Add("currency", "usd");
+                        headers.Add("transfer", "abroad");
+
+                        var properties = channel.CreateBasicProperties();
+                        properties.Headers = headers;
+
+                        string message = $"[{count}] sent with headers: [currencu:usd] [transfer:abroad]";
+
+                        var body = Encoding.UTF8.GetBytes(message);
+
+                        channel.BasicPublish(exchange: "headers_ex",
+                                            routingKey: "doesnt metter",
+                                            basicProperties: properties,
+                                            body: body);
+
+                        Console.WriteLine($"[{count++} USD transfer from abroad]");
+                    }
+                } while (true);
+            });
+
+            Task.Run(() =>
+            {
+                do
+                {
+                    int timeToSleep = new Random().Next(2000, 5000);
+                    Thread.Sleep(timeToSleep);
+
+                    var factory = new ConnectionFactory() { HostName = "localhost" };
+                    using (var connection = factory.CreateConnection())
+                    using (var channel = connection.CreateModel())
+                    {
+                        channel.ExchangeDeclare(exchange: "headers_ex", type: ExchangeType.Headers);
+
+                        Dictionary<string, object> headers = new Dictionary<string, object>();
+                        headers.Add("currency", "usd");
+                        headers.Add("transfer", "internal");
+
+                        var properties = channel.CreateBasicProperties();
+                        properties.Headers = headers;
+
+                        string message = $"[{count}] sent with headers: [currencu:usd] [transfer:internal]";
+
+                        var body = Encoding.UTF8.GetBytes(message);
+
+                        channel.BasicPublish(exchange: "headers_ex",
+                                            routingKey: "doesnt metter",
+                                            basicProperties: properties,
+                                            body: body);
+
+                        Console.WriteLine($"[{count++} USD transfer internal]");
+                    }
+                } while (true);
+            });
+
+            Task.Run(() =>
+            {
+                do
+                {
+                    int timeToSleep = new Random().Next(2000, 6000);
+                    Thread.Sleep(timeToSleep);
+
+                    var factory = new ConnectionFactory() { HostName = "localhost" };
+                    using (var connection = factory.CreateConnection())
+                    using (var channel = connection.CreateModel())
+                    {
+                        channel.ExchangeDeclare(exchange: "headers_ex", type: ExchangeType.Headers);
+
+                        Dictionary<string, object> headers = new Dictionary<string, object>();
+                        headers.Add("currency", "eur");
+                        headers.Add("transfer", "internal");
+
+                        var properties = channel.CreateBasicProperties();
+                        properties.Headers = headers;
+
+                        string message = $"[{count}] sent with headers: [currencu:usd] [transfer:internal]";
+
+                        var body = Encoding.UTF8.GetBytes(message);
+
+                        channel.BasicPublish(exchange: "headers_ex",
+                                            routingKey: "doesnt metter",
+                                            basicProperties: properties,
+                                            body: body);
+
+                        Console.WriteLine($"[{count++} EUR transfer internal]");
+                    }
+                } while (true);
+            });
+
+            Console.ReadKey();
+        }
+    }
+}
